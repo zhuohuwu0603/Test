d87f386d2ab8bda50ed13028ebcf2b01f15e5a2e
zhuohuawu0603 <zhuohuawu0603@gmail.com>
2014-04-21 22:23:55 -0400

    Add hello, wzhh
    Signed-off-by: zhuohuawu0603 <zhuohuawu0603@gmail.com>

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, world");
+		System.out.println("Hello, wzhh");
 	}
 }
