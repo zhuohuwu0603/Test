61d77dbfaae3149ae450e90c12ef7b87bdfe2b2f
zhuohuwu0603 <zhuohuawu0603@gmail.com>
2014-04-21 22:28:20 -0400

    Update HelloWorld.java

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,8 @@
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, world");
-		System.out.println("Hello, wzhh");
+		System.out.println("Hello, wzhh2");
 	}
 }
